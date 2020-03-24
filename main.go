e11697ea012fde2b02ee4e53ec17ef9dee0e64a4
Sullivan <prellwsr8859@uwec.edu>
2020-03-24 13:46:43 -0500

    Creating Account Struct

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+type User struct {
+	FName    string `json:"fname"`
+	LName    string `json:"lname"`
+	Email    string `json:"email"`
+	Type     string `json:"type"`
+	ID       int64  `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
 func main() {
 	//TODO: implement
 }
