9fb76d132d5c34507a91ab96ffcbdbc84875b9dc
Sullivan <prellwsr8859@uwec.edu>
2020-03-29 20:06:08 -0500

    Began work on token generation, token validation, and token renewal

--- token.go
+++ token.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/joho/godotenv"
 )
 
@@ -16,6 +18,65 @@ func getEnv() {
 
 }
 
-func genToken() {
+func genToken(email string) {
 	jwtSecret := os.Getenv("JWT_SECRET")
+	expirationTime := time.Now().Add(5 * time.Minute)
+	// Create the JWT claims, which includes the username and expiry time
+	claims := &Claims{
+		Email: email,
+		StandardClaims: jwt.StandardClaims{
+			// In JWT, the expiry time is expressed as unix milliseconds
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
+	// Declare the token with the algorithm used for signing, and the claims
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	// Create the JWT string
+	tokenString, err := token.SignedString(jwtSecret)
+	if err != nil {
+		//TODO: handle
+	}
+
+}
+
+func validateToken(tokenString string) {
+
+	// Initialize a new instance of `Claims`
+	claims := &Claims{}
+
+	// Parse the JWT string and store the result in `claims`.
+	// Note that we are passing the key in this method as well. This method will return an error
+	// if the token is invalid (if it has expired according to the expiry time we set on sign in),
+	// or if the signature does not match
+	tkn, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			//TODO: Handle
+		}
+	}
+	if !tkn.Valid {
+		//TODO: Handle
+	}
+}
+
+func renewToken(tokenString string, tokenExpire int) {
+	// We ensure that a new token is not issued until enough time has elapsed
+	// In this case, a new token will only be issued if the old token is within
+	// 30 seconds of expiry. Otherwise, return a bad request status
+	if time.Unix(tokenExpire, 0).Sub(time.Now()) > 30*time.Second {
+		// Not expired
+	} else {
+		// Now, create a new token for the current use, with a renewed expiration time
+		expirationTime := time.Now().Add(5 * time.Minute)
+		claims.ExpiresAt = expirationTime.Unix() // TODO: Change claims to DB work - this needs to exist in the database
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+		tokenString, err := token.SignedString(jwtKey)
+		if err != nil {
+			// TODO: Handle
+		}
+	}
+
 }
