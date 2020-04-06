17135c3758a25d44972f07ba462e8d8893935131
Sullivan <prellwsr8859@uwec.edu>
2020-04-06 01:23:48 -0500

    Restructured Login

--- login.go
+++ login.go
@@ -9,55 +9,55 @@ import (
 
 func LogIn(email string, password string, jwt string, collection *gocb.Collection) (UserToken, APIError) {
 	returnError := APIError{}
-	ops := []gocb.LookupInSpec{
-		gocb.GetSpec("passoword", &gocb.GetSpecOptions{}),
-		gocb.GetSpec("id", &gocb.GetSpecOptions{}),
-		gocb.GetSpec("token.token", &gocb.GetSpecOptions{}),
-		gocb.GetSpec("token.expiredate", &gocb.GetSpecOptions{}),
-	}
-	getResult, err := collection.LookupIn(email, ops, &gocb.LookupInOptions{})
-	if err != nil {
-		panic(err)
-		//TODO: Create API Err
-	}
-
 	var dbPassword string
 	var dbID string
 	var dbToken string
 	var dbExpireDate int64
-	err = getResult.ContentAt(0, &dbPassword)
-	if err != nil {
-		panic(err)
-		//TODO: Create API Err
-	}
-	err = getResult.ContentAt(1, &dbID)
-	if err != nil {
-		panic(err)
-		//TODO: Create API Err
-	}
-	err = getResult.ContentAt(2, &dbToken)
-	if err != nil {
-		panic(err)
-		//TODO: Create API Err
-	}
-	err = getResult.ContentAt(3, &dbExpireDate)
-	if err != nil {
-		panic(err)
-		//TODO: Create API Err
-	}
-	dbUserToken := UserToken{
-		Token:      dbToken,
-		ExpireDate: dbExpireDate,
-	}
-	if !ValidateToken(jwt, dbUserToken) {
-		// Need to login
-		userExists, errors := UserExist(email, collection)
-		if errors.Error {
-			//TODO: Handle error
-		} else if !userExists {
-			//TODO: Handle User doesn't exist
-		} else {
-			// User Exists and no errors!
+	var dbUserToken UserToken
+	userExists, errors := UserExist(email, collection)
+	if errors.Error {
+		//TODO: Handle error
+	} else if !userExists {
+		//TODO: Handle User doesn't exist
+	} else {
+		// User Exists and no errors!
+		ops := []gocb.LookupInSpec{
+			gocb.GetSpec("passoword", &gocb.GetSpecOptions{}),
+			gocb.GetSpec("id", &gocb.GetSpecOptions{}),
+			gocb.GetSpec("token.token", &gocb.GetSpecOptions{}),
+			gocb.GetSpec("token.expiredate", &gocb.GetSpecOptions{}),
+		}
+		getResult, err := collection.LookupIn(email, ops, &gocb.LookupInOptions{})
+		if err != nil {
+			panic(err)
+			//TODO: Create API Err
+		}
+
+		err = getResult.ContentAt(0, &dbPassword)
+		if err != nil {
+			panic(err)
+			//TODO: Create API Err
+		}
+		err = getResult.ContentAt(1, &dbID)
+		if err != nil {
+			panic(err)
+			//TODO: Create API Err
+		}
+		err = getResult.ContentAt(2, &dbToken)
+		if err != nil {
+			panic(err)
+			//TODO: Create API Err
+		}
+		err = getResult.ContentAt(3, &dbExpireDate)
+		if err != nil {
+			panic(err)
+			//TODO: Create API Err
+		}
+		dbUserToken = UserToken{
+			Token:      dbToken,
+			ExpireDate: dbExpireDate,
+		}
+		if !ValidateToken(jwt, dbUserToken) {
 
 			err = bcrypt.CompareHashAndPassword([]byte(dbPassword), []byte(password))
 			if err != nil {
@@ -83,8 +83,7 @@ func LogIn(email string, password string, jwt string, collection *gocb.Collectio
 				}
 			}
 		}
-	} else {
-		return dbUserToken, returnError
+
 	}
 	return dbUserToken, returnError
 }
