394f9172c212ac0b9b032bb535aa5a67ec2a14d5
Sullivan <prellwsr8859@uwec.edu>
2020-04-11 16:13:41 -0500

    Began implementation of creating classrooms

--- classroom.go
+++ classroom.go
@@ -1,5 +1,26 @@
 package main
 
-func CreateClassroom() {
+import (
+	"github.com/couchbase/gocb"
+	"github.com/graphql-go/graphql"
+)
+
+/*
+type Classroom struct {
+	CRID              string    `json:"crid"`
+	ProfessorEmail    string    `json:"professoremail"`
+	ProfessorID       string    `json:"professorid"`
+	DCordServerID     string    `json:"dcordserverid"`
+	DCordConnected    bool      `json:"dcordconnected"`
+}
+*/
+
+func CreateClassroomDiscord(params graphql.Params, collection gocb.Collection) {
+	// Classroom creation from the Discord Bot
+	// Information passed: Linking Token: Token contains: Classroom ID, prof email
+	// Prof Discord ID passed, Discord server ID
+}
+
+func CreateClassroomFrontEnd(params graphql.Params, collection gocb.Collection) {
 	//TODO: Implement
 }
